3f8f88dde3a6af1cb32ec0376725f3f539de2c1b
Sebastian Wilde Alarcon Arenas <sebastian.alarcon@ucsp.edu.pe>
2017-04-03 21:54:31 -0500

    Segundo trabajo, transformaciones

--- Trabajo2/main.cpp
+++ Trabajo2/main.cpp
@@ -24,6 +24,7 @@ GLvoid window_key(unsigned char key, int x, int y);
 //function called on each frame
 GLvoid window_idle();
 float a = 0;
+float c = 0;
 float b = -8;
 bool esfera = 1;
 int time = 0;
@@ -96,28 +97,57 @@ GLvoid window_display()
     time = glutGet(GLUT_ELAPSED_TIME); // recupera el tiempo ,que paso desde el incio de programa
     float dt = float(time -timebase)/1000.0;// delta time
     timebase = time;
-    glTranslated(b,0,0);
-    glutSolidSphere(1,50,50);
-    if (b<=-8) esfera = 1;
-    if (b>=8) esfera = 0;
-    if (esfera == 1) b+=0.01f;
-    else b-=0.01f;
-	/*dibujar aqui*/
-    glRotatef(a,1,0,0);
-    glTranslatef(0,5,0);
-    glutSolidCube(1.4);
+    /*Ejercicios del 1 al 4 */
 
+//    glTranslated(b,0,0);
+//    glutSolidSphere(1,50,50);
+//    if (b<=-8) esfera = 1;
+//    if (b>=8) esfera = 0;
+//    if (esfera == 1) b+=0.01f;
+//    else b-=0.01f;
+//	/*dibujar aqui*/
+//    glRotatef(a,1,0,0);
+//    glTranslatef(0,5,0);
+//    glutSolidCube(1.4);
+//
+//
+//	glPushMatrix();
+//    glRotatef(a,0,0,1);
+//    glTranslatef(10,0,0);
+//    a+=90*dt;
+//    glutSolidTeapot(2.0);
+//
+//    glRotatef(a*3,0,1,0);
+//    glTranslatef(3,0,0);
+//    glutSolidTorus(0.2,0.3,5,10);
+//    glPopMatrix();
 
-	glPushMatrix();
     glRotatef(a,0,0,1);
-    glTranslatef(10,0,0);
-    a+=90*dt;
-    glutSolidTeapot(2.0);
+    glColor3d(255,255,0);
+    glutSolidSphere(4,8,8); // el sol
 
-    glRotatef(a*3,0,1,0);
-    glTranslatef(3,0,0);
-    glutSolidTorus(0.2,0.3,5,10);
+
+
+    glPushMatrix();
+    glRotatef(3*a,0,0,1);
+    glTranslatef(10,0,0);
+    glColor3d(0,0,255);
+    glutSolidSphere(2,8,8); // la tierra
+
+    glRotatef(a*1.5,0,1,0);
+    glTranslatef(3.5,0,0);
+    glColor3d(215,215,215);
+    glutSolidSphere(1,8,8); //la luna
     glPopMatrix();
+
+    glRotatef(a,0,0,1);
+    glTranslatef(18,0,0);
+    glColor3d(254,0,0);
+    glutSolidSphere(2,8,8); // marte
+    c+=90*dt;
+    a+=0.03;
+    //a+=90*dt;
+
 	glutSwapBuffers();
 
 
